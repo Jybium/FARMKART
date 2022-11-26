5577663fb899d5a19e224f50e98b668b50284588
asaolu james <asaolujtomi@gmail.com>
2022-11-26 21:06:49 +0100

    scroll animation for index.html

--- templates/style.js
+++ templates/style.js
@@ -1,17 +1,17 @@
 // This is the modal for the signup modal box
-document.querySelector(".mini").addEventListener("click", function () {
-  document.querySelector("#myModal").style.display = "block";
-  document.querySelector(".over").style.display = "block";
-});
-document.querySelector(".close").addEventListener("click", function () {
-  document.querySelector("#myModal").style.display = "none";
-  document.querySelector(".over").style.display = "none";
-});
+// document.querySelector(".mini").addEventListener("click", function () {
+//   document.querySelector("#myModal").style.display = "block";
+//   document.querySelector(".over").style.display = "block";
+// });
+// document.querySelector(".close").addEventListener("click", function () {
+//   document.querySelector("#myModal").style.display = "none";
+//   document.querySelector(".over").style.display = "none";
+// });
 
-document.addEventListener("keydown", function (e) {
-  document.querySelector("#myModal").style.display = "none";
-  document.querySelector(".over").style.display = "none";
-});
+// document.addEventListener("keydown", function (e) {
+//   document.querySelector("#myModal").style.display = "none";
+//   document.querySelector(".over").style.display = "none";
+// });
 
 // This is the script for the landing page
 const observer = new IntersectionObserver((entries) => {
